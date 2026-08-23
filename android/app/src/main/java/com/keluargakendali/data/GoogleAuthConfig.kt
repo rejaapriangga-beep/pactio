a247582d0e128a961f3b83660e6907c8c8f0968b
rejaapriangga-beep <reja.apriangga@protonmail.com>
2026-08-23 14:28:26 +0000

    Perbaiki Client ID Google: pakai client tipe Web application yang benar
    Sebelumnya GOOGLE_WEB_CLIENT_ID salah diisi dengan ID milik client tipe
    Android (bukan Web application) - itu penyebab error nyata di HP:
    '[28444] Developer console is not set up correctly.' Client Web application
    baru sudah dibuat terpisah di Google Cloud Console, client Android yang
    lama tetap dipakai (untuk verifikasi SHA-1/package APK), tidak dihapus.

--- android/app/src/main/java/com/keluargakendali/data/GoogleAuthConfig.kt
+++ android/app/src/main/java/com/keluargakendali/data/GoogleAuthConfig.kt
@@ -7,4 +7,4 @@ package com.keluargakendali.data
  * sesungguhnya tetap terjadi di backend lewat tanda tangan token yang diterbitkan Google.
  * Client secret TIDAK pernah dipakai/disimpan di sini maupun di backend.
  */
-const val GOOGLE_WEB_CLIENT_ID = "646103704802-sq3sgp6hbd1ctqrmagflc8c107fuc1dk.apps.googleusercontent.com"
+const val GOOGLE_WEB_CLIENT_ID = "646103704802-0h1tdg8nnbufou0krg1nqnd4phhabqi9.apps.googleusercontent.com"
