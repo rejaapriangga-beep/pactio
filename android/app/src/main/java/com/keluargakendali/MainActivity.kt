8bcfd6d200e4d05fd4b8cd054e18263b25518915
rejaapriangga-beep <reja.apriangga@protonmail.com>
2026-08-27 15:47:19 +0000

    Perbaiki CI gagal: import fillMaxSize hilang di MainActivity.kt
    Box pembungkus baru untuk CoachMarkOverlay (lihat commit sebelumnya)
    pakai Modifier.fillMaxSize() tapi importnya belum ada - baru kepakai
    pertama kali di file ini (sebelumnya cuma fillMaxWidth). Ketahuan dari
    log CI: "Unresolved reference 'fillMaxSize'" di MainActivity.kt:143.

--- android/app/src/main/java/com/keluargakendali/MainActivity.kt
+++ android/app/src/main/java/com/keluargakendali/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
